45613ce5462b1b75f410918a5caef2e9a77c95ad
liguotianjin <liguotianjin@163.com>
2020-12-11 16:20:55 +0800

    222

--- myFirstWeb3Application.js
+++
@@ -1,7 +0,0 @@
-var Web3 = require('web3');
-var web3 = new Web3();
-web3.setProvider(new Web3.providers.HttpProvider("http://localhost:8545"));
-var accounts = web3.eth.accounts;
-console.info(accounts.toString());
-var balance_coinbase = web3.eth.getBalance(web3.eth.accounts[0]);
-//console.info(balance_conbase);
